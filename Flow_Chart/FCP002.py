5d06d7d38bd03a7cc89fc46269327c70c6571536
Deep Tiwari <tiwarideep7275@gmail.com>
2026-04-27 23:14:00 +0530

    Add files via upload

---
+++ Flow_Chart/FCP002.py
@@ -0,0 +1,12 @@
+# Write a program to take input from the user, and then print it to the output.
+# Test Case1:
+# Input:
+# 12Output:
+# 12
+
+
+n=int(input())
+def input_no(n):
+    return n
+print(input_no(n))
+
