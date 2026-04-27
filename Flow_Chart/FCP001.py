50561f917877f036db36c1e5d90dd557b1fde6ce
Deep Tiwari <tiwarideep7275@gmail.com>
2026-04-27 23:13:06 +0530

    Add files via upload

---
+++ Flow_Chart/FCP001.py
@@ -0,0 +1,40 @@
+# Write a program to take three variables to store your birth date, birth month, and birth year, respectively, and then print them one by
+# one in a specified order.
+# Test Case1:
+# Explanation:
+# If the birth date is 22, the birth is May and the birth year is 2001 then Output:
+# 22May
+# 2001
+
+
+# DeepLarry Approach
+
+
+def birth_date(date=22,
+    month="May",
+    year=2004):
+    print(str(date) + month)
+    print(year)
+birth_date()
+
+
+#-----------------------------------------------------****Basics
+# input function se values lena
+# function me pass karna
+# return use karna (instead of print) 
+
+
+
+# Task:
+
+# user se ek number lo
+# function me pass karo
+# function uska square return kare
+# bahar print karo
+
+n=int(input())
+def square_no(n):
+    return(n**2)
+print(square_no(n))
+
+#--------------------DeepLarry------------------#
