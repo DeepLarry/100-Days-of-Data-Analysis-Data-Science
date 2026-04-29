4c10ca33bdb6e5b5e36fdabc791f5907b74e9d6a
Deep Tiwari <tiwarideep7275@gmail.com>
2026-04-29 14:58:01 +0530

    Add files via upload

---
+++ Flow_Chart/FCP007.py
@@ -0,0 +1,12 @@
+# Write a program to convert a temperature from Celsius to Fahrenheit using the formula C/5 = (F-32)/9
+# Test Case1:
+# Input:
+# 30 Output:
+# 86
+
+c=int(input())
+def celsius_to_Fahrenheit():
+    F=9*c/5+32
+    print(F)
+celsius_to_Fahrenheit()
+
