e39241a4bd63143dedd4d53e4180db5796fb8ad8
Deep Tiwari <tiwarideep7275@gmail.com>
2026-04-29 14:58:28 +0530

    Add files via upload

---
+++ Flow_Chart/FCP008.py
@@ -0,0 +1,16 @@
+# Write a program to take two inputs from the user and swap them without using a temporary or third variable.
+# Test Case1:
+# Input:
+# 2
+# 3Output:
+# 3
+# 2 
+
+a,b=map(int,input().split())
+def swaping_num(
+    a=b,
+    b=a
+):
+    print(a,b)
+swaping_num()
+
