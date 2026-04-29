3301796341a63ae56fc79a2613081ff07e7aa605
Deep Tiwari <tiwarideep7275@gmail.com>
2026-04-29 14:57:30 +0530

    Add files via upload

---
+++ Flow_Chart/FCP006.py
@@ -0,0 +1,16 @@
+# Write a program to take a positive number from the user and then display the last digit of that number.
+
+# Test Case1:
+# Input:
+# 843 Output:
+# 3
+# Test Case2:
+# Input:
+# 321 Output:
+# 1 
+N=int(input())
+def last_digit():
+    X=N%10
+    print(X)
+last_digit()
+  
