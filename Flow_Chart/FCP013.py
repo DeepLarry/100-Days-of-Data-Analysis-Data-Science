67c9bb8955dde37a15e8d7320db47ed12fd9de60
Deep Tiwari <tiwarideep7275@gmail.com>
2026-05-01 23:08:20 +0530

    Add files via upload

---
+++ Flow_Chart/FCP013.py
@@ -0,0 +1,12 @@
+# Write a program to take an integer as input and print the smallest positive integer that is a multiple of both 2 and n.
+# Test Case:
+# Input:
+# 5
+# Output:
+# 10
+# Test Case:
+# Input:
+# 6
+# Output:
+# 6
+
